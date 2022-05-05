52e6a5469ec0531c1172b28f99a6a0e6df08f3a4
Rusnac-Ivan <rusnacivan62@gmail.com>
2022-05-05 16:45:15 +0300

    minor changes

--- Sources/engine/GLObjects/CubeMap.h
+++ Sources/engine/GLObjects/CubeMap.h
@@ -2,6 +2,13 @@
 #define _CUBE_MAP_H_
 
 #include <GLObjects/Texture.h>
+#include <string>
+#include <GLObjects/Program.h>
+#include <GLObjects/VertexBuffer.h>
+#include <GLObjects/VertexArray.h>
+#include <glm/glm.hpp>
+
+
 namespace gl
 {
 	class CubeMap : public Texture
@@ -9,12 +16,12 @@ namespace gl
 	public:
 		enum Target
 		{
-			RIGHT	= GL_TEXTURE_CUBE_MAP_POSITIVE_X,
-			LEFT	= GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
-			TOP		= GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-			BOTTOM	= GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-			BACK	=  GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
-			FRONT	= GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
+			POSITIVE_X = GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+			NEGATIVE_X = GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+			POSITIVE_Y = GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+			NEGATIVE_Y = GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+			POSITIVE_Z =  GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
+			NEGATIVE_Z = GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
 		};
 
 		struct Sampler
@@ -23,6 +30,7 @@ namespace gl
 			FilterMode minFilter;
 			WrapMode wrapModeS;
 			WrapMode wrapModeT;
+			WrapMode wrapModeR;
 			bool generateMipmaps;
 		};
 	private:
@@ -30,22 +38,29 @@ namespace gl
 		static constexpr Type mTarget = Type::TARGET_CUBE_MAP;
 
 		Sampler mSampler;
+
+		Program mProgram;
+
+		VertexBuffer mVBO;
+		VertexArray mVAO;
+
 	public:
 		CubeMap();
 		~CubeMap();
 
+		void SetPositiveX(std::string file_name);
+		void SetNegativeX(std::string file_name);
+		void SetPositiveY(std::string file_name);
+		void SetNegativeY(std::string file_name);
+		void SetPositiveZ(std::string file_name);
+		void SetNegativeZ(std::string file_name);
+
+		void SetSampler(const Sampler& sampler);
+
+		void Draw(const glm::mat4& view, const glm::mat4& proj);
 	private:
 
 	};
-
-	CubeMap::CubeMap()
-	{
-	}
-
-	CubeMap::~CubeMap()
-	{
-
-	}
 }
 
 
